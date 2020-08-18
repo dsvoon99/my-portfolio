341fcc086c54c8da5c83587035fff877e593eee6
Voon Shun Zhi <voonshunzhi@Voons-MacBook-Air.local>
2020-08-19 01:47:44 +0800

    Refine Sticky Navbar

--- StickyBar.js
+++ StickyBar.js
@@ -1,15 +1,29 @@
 class StickyBar {
     constructor() {
+        this.scrollTimer = null;
+        this.mouseenter = false;
+        var self = this;
         $(window).scroll(function () {
             $(window).scroll(function() {
-                var self = this;
-                console.log(this)
-                clearTimeout($.data(self, 'scrollTimer'));
-                $.data(self, 'scrollTimer', setTimeout(function() {
+                clearTimeout(self.scrollTimer);
+                self.scrollTimer = setTimeout(function() {
                     $('.navbar__fixed').removeClass('navbar__fixed--visible');
-                }, 2000));
+                }, 2000)
                 $('.navbar__fixed').mouseenter(function() {
-                    clearTimeout($.data(self, 'scrollTimer')); 
+                    if(self.mouseenter) {
+                        return;
+                    }
+                    clearTimeout(self.scrollTimer);
+                    this.mouseenter = true;
+                })
+                $('.navbar__fixed').mouseleave(function() {
+                    if(!this.mouseenter) {
+                        return;
+                    }
+                    self.scrollTimer = setTimeout(function() {
+                        $('.navbar__fixed').removeClass('navbar__fixed--visible');
+                    }, 2000)
+                    this.mouseenter = false
                 })
                 if ($(window).scrollTop() > 1000) {
                     $('.navbar__fixed').addClass('navbar__fixed--visible');
