67e7866d95e86afe02f6d19d3d6813570df3fe39
Voon Shun Zhi <voonshunzhi@Voons-MacBook-Air.local>
2020-08-18 15:03:32 +0800

    Mobile Responsive Kaodim

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Can also be included with a regular script tag
 import Typed from 'typed.js';
+import SmoothScroll from 'smooth-scroll';
 
 var options = {
   strings: ["Mobile Developer . Machine Learning . Content Creator", "Mobile Developer . Machine Learning . Content Creator", "Mobile Developer . Machine Learning . Content Creator"],
@@ -10,3 +11,4 @@ var options = {
 };
 
 var typed = new Typed('#title', options);
+var scroll = new SmoothScroll('a[href*="#"]');
