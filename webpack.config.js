22116a167749c79e5ef2efe172ab0ce24a73a873
Voon Shun Zhi <voonshunzhi@Voons-MacBook-Air.local>
2020-08-21 02:12:17 +0800

    New Folder Change to adapt to s3 policy

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './index.js',
+  entry: 'src/index.js',
   output: {
     filename: 'script.js',
     path: path.resolve(__dirname, 'assets'),
